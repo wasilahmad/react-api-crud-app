ff7ac428d94cb8e681eeb1d145a931be34e92715
wasilahmad <wasilinfo@gmail.com>
2020-03-21 23:57:40 +0530

    Updates

---
+++ precache-manifest.cfe3f33b51c984a6b072804da2f0b69e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7d52c81308a72ba4f37276b1b0f809",
+    "url": "/react-api-crud-app/index.html"
+  },
+  {
+    "revision": "97aab57a6cf8670761ae",
+    "url": "/react-api-crud-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a274f495c1c74d042509",
+    "url": "/react-api-crud-app/static/css/main.52f3e5db.chunk.css"
+  },
+  {
+    "revision": "97aab57a6cf8670761ae",
+    "url": "/react-api-crud-app/static/js/2.451e9413.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/react-api-crud-app/static/js/2.451e9413.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a274f495c1c74d042509",
+    "url": "/react-api-crud-app/static/js/main.b00c92e8.chunk.js"
+  },
+  {
+    "revision": "53b884b2e5696e54c5ec",
+    "url": "/react-api-crud-app/static/js/runtime-main.709eb026.js"
+  }
+]);
